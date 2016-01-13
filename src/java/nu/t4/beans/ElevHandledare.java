2bb887cfa22e43a3a3f904a29c9464b31d7f77ad
Maikii <maikrichardwagner@gmail.com>
2016-01-13 15:38:08 +0100

    Json filer till elever

---
+++ src/java/nu/t4/beans/ElevHandledare.java
@@ -0,0 +1,57 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package nu.t4.beans;
+
+import com.mysql.jdbc.Connection;
+import com.mysql.jdbc.Statement;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import javax.ejb.Stateless;
+import javax.json.Json;
+import javax.json.JsonArray;
+import javax.json.JsonArrayBuilder;
+
+/**
+ *
+ * @author maikwagner
+ */
+@Stateless
+public class ElevHandledare {
+
+    public JsonArray getElev() {
+        try {
+            Connection conn = (Connection) DriverManager.getConnection("jdbc:mysql://10.97.72.5/aplapp", "aplapp", "Teknikum123");
+            Statement stmt = (Statement) conn.createStatement();
+            String sql = "SELECT * FROM skolans_användare WITH behörighet=0";
+            ResultSet data = stmt.executeQuery(sql);
+
+            JsonArrayBuilder elever = Json.createArrayBuilder();
+
+            while (data.next()) {
+                elever.add(Json.createObjectBuilder()
+                        .add("ID", data.getInt("ID"))
+                        .add("namn", data.getString("namn"))
+                        .add("Telefonnummer", data.getString("Telefonnummer"))
+                        .add("email", data.getString("email"))
+                        .add("klass", data.getInt("klass"))
+                        .add("handledare_ID", data.getInt("handledare_ID"))
+                        .add("senast_inloggad", data.getInt("senast_inloggad"))
+                        .add("behörighet", false)
+                        .build());
+            }
+    
+            conn.close();
+            return elever.build();
+          
+        } catch (Exception e) {
+            System.out.println("elevhandledare - getElev()");
+            System.out.println(e.getMessage());
+            return null;
+        }
+
+    }
+
+}
---
+++ src/java/nu/t4/services/ElevService.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package nu.t4.services;
+
+import nu.t4.beans.ElevHandledare;
+import javax.ejb.EJB;
+import javax.inject.Named;
+import javax.json.JsonArray;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+/**
+ *
+ * @author maikwagner
+ */
+@Named("elev")
+public class ElevService {
+  
+    @EJB
+    ElevHandledare elevHandledare;
+    
+    @GET
+    @Path("/elev/{id}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response getElev(@PathParam("id") int id){
+        JsonArray elever = elevHandledare.getElev();
+        if(elever != null){
+            return Response.ok(elever).build();
+        }else{
+            return Response.status(Response.Status.SERVICE_UNAVAILABLE).build();
+        }
+    }
+    
+    
+}
