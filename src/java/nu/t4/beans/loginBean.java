74ae5d8433912326632d65dc6cfab14566182224
dannil1212 <dannil1212@utb.vaxjo.se>
2016-01-13 10:41:48 +0100

    inloggning klar

---
+++ src/java/nu/t4/beans/loginBean.java
@@ -0,0 +1,80 @@
+package nu.t4.beans;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+import com.mysql.jdbc.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import javax.faces.bean.ManagedBean;
+import org.mindrot.jbcrypt.BCrypt;
+
+/**
+ *
+ * @author Daniel Nilsson
+ */
+@ManagedBean
+public class loginBean {
+
+    private String username;
+
+    private String password;
+    
+    private boolean loggedIn;
+
+    public boolean isLoggedIn() {
+        return loggedIn;
+    }
+
+    public void setLoggedIn(boolean loggedIn) {
+        this.loggedIn = loggedIn;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String login() {
+        
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            Connection conn = (Connection) DriverManager.getConnection("jdbc:mysql://localhost/aplapp", "root", "");
+            Statement stmt = conn.createStatement();
+            String sql = String.format("SELECT * FROM adminuser WHERE username = '%s'", username);
+            ResultSet data = stmt.executeQuery(sql);
+            
+            data.next();//ta ut första raden
+            String hashedpwd = data.getString("password");
+            if (BCrypt.checkpw(password, hashedpwd)) {
+                conn.close();
+                loggedIn = true;
+                return "main";
+            } else {
+                conn.close();
+                loggedIn = false;
+                return "index";
+            }
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            loggedIn = false;
+            return "index";
+        }
+    }
+}
---
+++ src/java/nu/t4/beans/menuBean.java
@@ -0,0 +1,28 @@
+package nu.t4.beans;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+/**
+ *
+ * @author Daniel Nilsson
+ */
+public class menuBean {
+    private String menuString;
+
+    public String getMenuString() {
+        return menuString;
+    }
+
+    public void setMenuString(String menuString) {
+        this.menuString = menuString;
+    }
+    
+    public void addClassFunction(){
+        menuString ="";
+    }
+}
