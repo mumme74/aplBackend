415362e2a16269e9d9e91402691986d7d3a288a3
dannil1212 <dannil1212@utb.vaxjo.se>
2016-01-21 14:35:25 +0100

    testat att lägga till/radera klasser

---
+++ test/nu/t4/beans/AdminManagerTest.java
@@ -0,0 +1,119 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package nu.t4.beans;
+
+import com.mysql.jdbc.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.List;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Daniel Nilsson
+ */
+public class AdminManagerTest {
+
+    public AdminManagerTest() {
+    }
+
+    @BeforeClass
+    public static void setUpClass() {
+    }
+
+    @AfterClass
+    public static void tearDownClass() {
+    }
+
+    @Before
+    public void setUp() {
+    }
+
+    @After
+    public void tearDown() {
+    }
+
+    /**
+     * Test of addClass method, of class AdminManager.
+     */
+    @Test
+    public void testAddClass() {
+        System.out.println("addClass");
+        String expResult = "EttTest";
+        try {
+            String url = "jdbc:mysql://10.97.72.5/aplapp";
+            String user = "aplapp";
+            String password = "Teknikum123";
+            Connection conn = (Connection) DriverManager.getConnection(url, user, password);
+            Statement stmt = conn.createStatement();
+
+            String sql = "INSERT INTO klass VALUES(NULL, 'EttTest', 1)";
+            stmt.executeUpdate(sql);
+            conn.close();
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+        String result = "";
+        try {
+            String url = "jdbc:mysql://10.97.72.5/aplapp";
+            String user = "aplapp";
+            String password = "Teknikum123";
+            Connection conn = (Connection) DriverManager.getConnection(url, user, password);
+            Statement stmt = conn.createStatement();
+
+            String sql = "SELECT namn FROM klass WHERE namn = 'EttTest'";
+            ResultSet data = stmt.executeQuery(sql);
+            data.next();
+            result = data.getString("namn");
+        } catch (Exception e) {
+        }
+        
+        assertEquals(expResult, result);
+        
+        System.out.println("deleteClass");
+        expResult = "";
+        try {
+            String url = "jdbc:mysql://10.97.72.5/aplapp";
+            String user = "aplapp";
+            String password = "Teknikum123";
+            Connection conn = (Connection) DriverManager.getConnection(url, user, password);
+            Statement stmt = conn.createStatement();
+
+            String sql = "DELETE FROM klass WHERE namn ='EttTest'";
+            stmt.executeUpdate(sql);
+            conn.close();
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+        result = "";
+        try {
+            String url = "jdbc:mysql://10.97.72.5/aplapp";
+            String user = "aplapp";
+            String password = "Teknikum123";
+            Connection conn = (Connection) DriverManager.getConnection(url, user, password);
+            Statement stmt = conn.createStatement();
+
+            String sql = "SELECT namn FROM klass WHERE namn = 'EttTest'";
+            ResultSet data = stmt.executeQuery(sql);
+            data.next();
+            result = data.getString("namn");
+        } catch (Exception e) {
+        }
+        
+        assertEquals(expResult, result);
+    }
+    
+    
+    
+    
+
+}
