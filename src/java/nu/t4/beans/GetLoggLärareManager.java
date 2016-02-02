63d964d11c3d8c726feb51f01efb72c11bec8973
LudvigAndersson <gulual_96@hotmail.com>
2016-02-02 23:45:01 +0100

    GetLoggLärareManager och Service

---
+++ src/java/nu/t4/beans/GetLoggLärareManager.java
@@ -0,0 +1,54 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package nu.t4.beans;
+
+import com.mysql.jdbc.Connection;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import javax.json.Json;
+import javax.json.JsonArray;
+import javax.json.JsonArrayBuilder;
+
+/**
+ *
+ * @author luan96001
+ */
+public class GetLoggLärareManager {
+     public JsonArray getLoggar(int elev_id){
+         
+        try {
+            
+             Connection conn = ConnectionFactory.getConnection("local");
+            Statement stmt = conn.createStatement();
+                     String sql = String.format("SELECT * FROM loggbok WHERE elev_id = %d", elev_id);
+            ResultSet data = stmt.executeQuery(sql);
+            data.next();
+            
+            JsonArrayBuilder jsonArray = Json.createArrayBuilder();
+            while(data.next()){
+                
+                jsonArray.add(Json.createObjectBuilder()
+                        .add("ID", data.getInt("ID"))
+                        .add("elev_id", data.getInt("elev_id"))
+                        .add("innehall", data.getString("innehåll"))
+                        .add("intryck", data.getInt("intryck"))
+                        .add("datum", data.getInt("datum"))
+                        .add("bild_id", data.getInt("bild_id"))
+                        .build());
+            }
+            
+            conn.close();
+            return jsonArray.build();
+            
+            
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+           
+            return null;
+        }
+    }
+    
+}
---
+++ src/java/nu/t4/services/GetLoggLärareService.java
@@ -0,0 +1,73 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package nu.t4.services;
+
+import com.google.api.client.googleapis.auth.oauth2.GoogleIdToken;
+import java.io.StringReader;
+import javax.ejb.EJB;
+import javax.json.Json;
+import javax.json.JsonArray;
+import javax.json.JsonObject;
+import javax.json.JsonReader;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import nu.t4.beans.APLManager;
+import nu.t4.beans.GetLoggLärareManager;
+
+/**
+ *
+ * @author luan96001
+ */
+public class GetLoggLärareService {
+    @EJB
+    APLManager manager;
+    
+    @EJB
+    GetLoggLärareManager loggManager;
+    
+    @GET
+    @Path("/allaLoggar")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response getLoggar(String body){
+        //Skapa ett json objekt av indatan
+        JsonReader jsonReader = Json.createReader(new StringReader(body));
+        JsonObject jsonObject = jsonReader.readObject();
+        jsonReader.close();
+
+        String idTokenString = null;
+        int användar_id = 0;
+        try {
+            idTokenString = jsonObject.getString("id");
+        } catch (Exception e) {
+            return Response.status(Response.Status.BAD_REQUEST).build();
+        }
+        
+        if (idTokenString != null) {
+            GoogleIdToken.Payload payload = manager.googleAuth(idTokenString);
+            if (payload != null) {
+                JsonObject användare = manager.getGoogleUser(payload.getSubject());
+                if (användare != null) {
+                    användar_id = användare.getInt("id");
+                } else {
+                    return Response.status(Response.Status.PRECONDITION_FAILED).build();
+                }
+            } else {
+                return Response.status(Response.Status.UNAUTHORIZED).build();
+            }
+        }
+        
+        JsonArray data = loggManager.getLoggar(användar_id);
+        if (data != null) {
+            return Response.ok(data).build();
+        } else {
+            return Response.serverError().build();
+        }
+    }
+    
+}
