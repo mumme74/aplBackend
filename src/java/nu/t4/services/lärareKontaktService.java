3f1ca6e3f162a0901df9371f9370382db6c085c3
dannil1212 <dannil1212@utb.vaxjo.se>
2016-02-09 10:49:55 +0100

    fixat lärarekontakt

--- src/java/nu/t4/services/lärareKontaktService.java
+++ src/java/nu/t4/services/lärareKontaktService.java
@@ -9,6 +9,7 @@ import com.google.api.client.googleapis.auth.oauth2.GoogleIdToken;
 import javax.ejb.EJB;
 import javax.json.JsonArray;
 import javax.json.JsonObject;
+import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.Context;
@@ -25,15 +26,18 @@ import nu.t4.beans.lärareKontaktManager;
 @Path("larare")
 public class lärareKontaktService {
     
+    
+    
     @EJB
     APLManager manager;
-    
+
     @EJB
     lärareKontaktManager lararManager;
-    
+
+    @GET
     @Path("/kontakt")
     @Produces(MediaType.APPLICATION_JSON)
-    public Response kontaktLärare(@Context HttpHeaders headers){
+    public Response kontaktLärare(@Context HttpHeaders headers) {
         //Kollar att inloggningen är ok
         String idTokenString = headers.getHeaderString("Authorization");
         System.out.println(idTokenString);
@@ -47,7 +51,7 @@ public class lärareKontaktService {
             return Response.status(Response.Status.UNAUTHORIZED).build();
         }
         int klass_id = elev.getInt("klass");
-        
+
         JsonArray data = lararManager.getKontaktLärare(klass_id);
         if (data != null) {
             return Response.ok(data).build();
