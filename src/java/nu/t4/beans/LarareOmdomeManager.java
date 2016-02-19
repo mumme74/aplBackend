e7126c10ce8a52206583c136f2f6b2575b2966b3
Maikii <maikrichardwagner@gmail.com>
2016-02-19 11:53:27 +0100

    oversikt

--- src/java/nu/t4/beans/LarareOmdomeManager.java
+++ src/java/nu/t4/beans/LarareOmdomeManager.java
@@ -7,6 +7,7 @@ package nu.t4.beans;
 
 import com.mysql.jdbc.Connection;
 import com.mysql.jdbc.Statement;
+import java.math.BigDecimal;
 import java.sql.ResultSet;
 import javax.ejb.Stateless;
 import javax.json.Json;
@@ -21,10 +22,11 @@ import javax.json.JsonArrayBuilder;
 @Stateless
 public class LarareOmdomeManager {
     public JsonArray getOmdome(int id) {
+        String sql =""; 
         try {
             Connection conn = ConnectionFactory.getConnection();
             Statement stmt = (Statement) conn.createStatement();
-            String sql = String.format(
+            sql = String.format(
                     "SELECT count(CASE WHEN intryck = 0 THEN 1 ELSE NULL END) as antal0,count(CASE WHEN intryck = 1 THEN 1 ELSE NULL END) as antal1,count(CASE WHEN intryck = 2 THEN 1 ELSE NULL END) as antal2 FROM loggbok WHERE elev_id = %d",id
             );
             ResultSet data = stmt.executeQuery(sql);
@@ -44,7 +46,9 @@ public class LarareOmdomeManager {
         } catch (Exception e) {
             System.out.println("LarareOmdomeManager");
             System.out.println(e.getMessage());
-            return null;
+            JsonArrayBuilder error = Json.createArrayBuilder();
+            error.add(Json.createObjectBuilder().add("sql",sql).add("Error", e.getMessage()).build());
+            return error.build();
         }
 
     }
